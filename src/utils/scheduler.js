4dc3a3f8aca0895687b83a43a4a624be6263ba23
Ivaskiv <viraivaskiv@gmail.com>
2025-08-20 22:07:31 +0200

    update

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -1,6 +1,8 @@
+// src/utils/scheduler.js
 import cron from "node-cron";
-import { bot } from "../../index.js";
-import { getAllActiveUsers } from "./airtable.js";
+import { bot } from "../../server.js";
+
+import AirtableService from "../services/airtableService.js";
 import reflectionService from "../services/reflectionService.js";
 
 export const initScheduler = () => {
@@ -8,9 +10,9 @@ export const initScheduler = () => {
   cron.schedule("0 8 * * *", async () => {
     console.log("🕗 Надсилання ранкових питань (усім активним користувачам)");
     try {
-      const activeUsers = await getAllActiveUsers();
+      const activeUsers = await AirtableService.getActiveUsers();
       for (const user of activeUsers) {
-        const tgId = user.fields.TG_id;
+        const tgId = user.TG_id;
         console.log(`Надсилаю ранкові питання користувачу ${tgId}`);
         await reflectionService.startDailyQuestions(bot, tgId, "morning");
       }
@@ -21,12 +23,12 @@ export const initScheduler = () => {
   }, { timezone: "Europe/Kiev" });
 
   // 22:00 вечірні
-  cron.schedule("0 22 * * *", async () => {
+  cron.schedule("10 22 * * *", async () => {
     console.log("🕣 Надсилання вечірніх питань (усім активним користувачам)");
     try {
-      const activeUsers = await getAllActiveUsers();
+      const activeUsers = await AirtableService.getActiveUsers();
       for (const user of activeUsers) {
-        const tgId = user.fields.TG_id;
+        const tgId = user.TG_id;
         console.log(`Надсилаю вечірні питання користувачу ${tgId}`);
         await reflectionService.startDailyQuestions(bot, tgId, "evening");
       }
