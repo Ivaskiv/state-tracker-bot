d96ea2260f1f019f93190c091a65f9d2ffc53e54
Ivaskiv <viraivaskiv@gmail.com>
2025-08-21 10:17:42 +0200

    update

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -6,7 +6,7 @@ import reflectionService from "../services/reflectionService.js";
 
 export const initScheduler = () => {
   // 08:00 ранкові
-  cron.schedule("40 08 * * *", async () => {
+  cron.schedule("20 10 * * *", async () => {
     console.log("🕗 Надсилання ранкових питань (усім активним користувачам)");
     try {
       const activeUsers = await airtableService.getActiveUsers();
